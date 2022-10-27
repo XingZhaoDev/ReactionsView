d1e2b3628eee596d1eeb3206f8092223cd186df1
Xing Zhao <zhaoxing8325@gmail.com>
2022-10-26 21:32:49 -0700

    Initial Commit

---
+++ ReactionsView/ReactionsViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactionsViewApp.swift
+//  ReactionsView
+//
+//  Created by XING ZHAO on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct ReactionsViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
